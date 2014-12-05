aad20e35237fcd9d8adde6fe9d182522b7747718
Andrew <andrewjamest1993@gmail.com>
2014-12-05 14:23:35 -0600

    "Now fetches real data on launch"

--- app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -31,7 +31,7 @@ import java.util.Date;
 import java.util.List;
 
 /**
- * Created by Andrew on 11/20/2014.
+ * Created by Andrew on 11/20/2014. Based on Udacity tutorial
  */
 public  class ForecastFragment extends Fragment {
 
@@ -132,7 +132,7 @@ public  class ForecastFragment extends Fragment {
         // it must be converted to milliseconds in order to be converted to valid date.
         Date date = new Date(time * 1000);
         SimpleDateFormat format = new SimpleDateFormat("E, MMM d");
-        return format.format(date).toString();
+        return format.format(date);
     }
 
     /**
@@ -143,8 +143,7 @@ public  class ForecastFragment extends Fragment {
         long roundedHigh = Math.round(high);
         long roundedLow = Math.round(low);
 
-        String highLowStr = roundedHigh + "/" + roundedLow;
-        return highLowStr;
+        return roundedHigh + "/" + roundedLow;
     }
 
     /**
@@ -165,8 +164,6 @@ public  class ForecastFragment extends Fragment {
         final String OWM_MIN = "min";
         final String OWM_DATETIME = "dt";
         final String OWM_DESCRIPTION = "main";
-
-        final String LOG_TAG = FetchWeatherTask.class.getSimpleName();
 
         JSONObject forecastJson = new JSONObject(forecastJsonStr);
         JSONArray weatherArray = forecastJson.getJSONArray(OWM_LIST);
@@ -261,7 +258,7 @@ public  class ForecastFragment extends Fragment {
 
                 // Read the input stream into a String
                 InputStream inputStream = urlConnection.getInputStream();
-                StringBuffer buffer = new StringBuffer();
+                StringBuilder buffer = new StringBuilder();
                 if (inputStream == null) {
                     // Nothing to do.
                     return null;
@@ -273,7 +270,7 @@ public  class ForecastFragment extends Fragment {
                     // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
                     // But it does make debugging a *lot* easier if you print out the completed
                     // buffer for debugging.
-                    buffer.append(line + "\n");
+                    buffer.append(line).append("\n");
                 }
 
                 if (buffer.length() == 0) {
